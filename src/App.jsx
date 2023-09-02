843b7ef3e4d57b86b64e38e0e03ad77d8f45797d
Anne Massicotte <anne.massicotte@hotmail.com>
2023-09-02 19:47:23 -0400

    Setup de la page App.jsx avec routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import './App.css'
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Route, Routes } from "react-router-dom";
+import AllCategories from "./meals/components/AllCategories";
+import Category from "./meals/components/Category";
+import Category from "./meals/components/Meal";
+
+const client = new QueryClient();
 
 function App() {
-
-
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<QueryClientProvider client={client}>
+			<Routes>
+				<Route path="/" element={<AllCategories />} />
+				<Route path="/category/:name" element={<Category />} />
+				<Route path="/meal/:id" element={<Meal />} />
+			</Routes>
+		</QueryClientProvider>
+	);
 }
 
-export default App
+export default App;
