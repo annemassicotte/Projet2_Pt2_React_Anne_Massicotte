4a27c3c7ec9d6734fd5f9f3d6ee250c17cf7c984
Anne Massicotte <anne.massicotte@hotmail.com>
2023-09-03 19:44:35 -0400

    Style AllCategories.jsx

--- src/meals/components/AllCategories.jsx
+++ src/meals/components/AllCategories.jsx
@@ -1,6 +1,9 @@
 import MealService from "../service/MealService";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const mealService = new MealService();
 
@@ -14,16 +17,25 @@ const AllCategories = () => {
 	if (isError) return <div>Error: {error.message}</div>;
 
 	return (
-		<>
-			<h1>Categories</h1>
-			<div>
-				{data && data.categories.map((category) => (
-					<Link to={`/category/${category.strCategory}`} key={category.idCategory}>
-						<p>{category.strCategory}</p>
-					</Link>
-				))}
-			</div>
-		</>
+		<Container className="d-flex flex-column justify-content-center container">
+			<Row className="bg-white p-3 rounded-4">
+				<h1 className="mb-4 fw-bold">Categories</h1>
+				<Col className="px-0">
+					<ul>
+						{data &&
+							data.categories.map((category) => (
+								<li className="fs-4">
+									<Link
+										to={`/category/${category.strCategory}`}
+										key={category.idCategory}>
+										{category.strCategory}
+									</Link>
+								</li>
+							))}
+					</ul>
+				</Col>
+			</Row>
+		</Container>
 	);
 };
 
